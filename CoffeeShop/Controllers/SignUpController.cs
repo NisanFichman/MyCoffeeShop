903c36cb461bec1f1c5d61aa3066a93120e1ed6f
Nisan Fichman <nisan92@gmail.com>
2022-01-22 20:27:57 +0200

    Coffee Web sitev using mvc

---
+++ CoffeeShop/Controllers/SignUpController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+using CoffeeShop.ViewModels;
+
+namespace CoffeeShop.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private CoffeeShopEntities db = new CoffeeShopEntities();
+
+        // GET: SignUp
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: SignUp/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: SignUp/Create
+        public ActionResult Create()
+        {
+            return View("Index");
+        }
+
+        // POST: SignUp/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,First_Name,Last_Name,Email,Password,Address,Date_Of_Birth,VIP")] User user)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return View("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: SignUp/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: SignUp/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,First_Name,Last_Name,Email,Password,Address,Date_Of_Birth,VIP")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: SignUp/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: SignUp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CoffeeShop/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public User u { get; set; }
+
+        public List<User> uList {get; set;}
+
+    }
+}
