985fe29c0eec402d4e8f3236483ac09eaef3e764
Nisan Fichman <nisan92@gmail.com>
2022-01-22 20:27:57 +0200

    Coffee Web site using mvc

---
+++ CoffeeShop/App_Start/RouteConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CoffeeShop
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "",
+                defaults: new { controller = "Home", action = "ShowHomePage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "ShowHome",
+                url: "Home/ShowHomePage",
+                defaults: new { controller = "Home", action = "ShowHomePage", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SignUp",
+                url: "Home/SignUp",
+                defaults: new { controller = "SignUp", action = "Enter", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+               name: "SignUp1",
+               url: "SignUp",
+               defaults: new { controller = "SignUp", action = "Enter", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "Home/Login",
+                defaults: new { controller = "Home", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Menu",
+               url: "Home/Menu",
+               defaults: new { controller = "Menus", action = "Index", id = UrlParameter.Optional }
+           );
+        }
+    }
+}
---
+++ CoffeeShop/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShop.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public User U1 { get; set; }
+
+        public List<User> UList {get; set;}
+
+    }
+}
